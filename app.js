7c0d11bac3d82dd06ab28db8be053fbdf26ef6a6
Bhavin1214 <bhavinmotka1234@gmail.com>
2025-04-17 00:20:38 +0530

    last optimization

--- app.js
+++ app.js
@@ -37,5 +37,5 @@ mongoose
   .catch((err) => console.error("MongoDB Connection Error:", err));
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT} 🚀`));
--- src/models/Booking.js
+++ src/models/Booking.js
@@ -5,7 +5,7 @@ const BookingSchema = new mongoose.Schema(
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     parkingId: { type: mongoose.Schema.Types.ObjectId, ref: "ParkingLocation", required: true },
     vehicleType: { type: String, enum: ["2-wheeler", "4-wheeler"], required: true },
-    vehicleNumber: { type: String, required: true }, // New field for vehicle number
+    vehicleNumber: { type: String, required: true }, 
     status: { type: String, enum: ["active", "completed", "cancelled"], default: "active" },
     startTime: { type: Date, required: true },
     endTime: { type: Date, required: true },
